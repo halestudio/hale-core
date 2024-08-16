4988996eacfb3902c5c4e1340a6b78c2eeec7ecb
Simon Templer <simon@wetransform.to>
2024-08-17 01:53:48 +0200

    chore: add editorconfig settings for Java and Groovy sources

--- buildSrc/src/main/groovy/HaleProjectExtension.groovy
+++ buildSrc/src/main/groovy/HaleProjectExtension.groovy
@@ -1,14 +1,17 @@
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class HaleProjectExtension {
 
-    // OSGI bundle options
-    String activator = null
-    boolean lazyActivation = false
-    String bundleName = null
-    String bundleSymbolicName = null
-    Boolean singletonBundle = null
-    String fragmentHost = null
-    String bundleVendor = null
-    String privatePackage = null
-    String exportPackage = null
-    boolean registeredBuddies = false
+	// OSGI bundle options
+	String activator = null
+	boolean lazyActivation = false
+	String bundleName = null
+	String bundleSymbolicName = null
+	Boolean singletonBundle = null
+	String fragmentHost = null
+	String bundleVendor = null
+	String privatePackage = null
+	String exportPackage = null
+	boolean registeredBuddies = false
 }
