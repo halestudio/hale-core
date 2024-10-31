02fa4bab86af98b6018cd213b3c7cbdeba789143
Florian Esser <fe@wetransform.to>
2024-10-31 10:13:35 +0100

    perf: make index join/merge handlers optional
    Restore the classes `JoinHandler` and `PropertiesMergeHandler` as the default
    handlers for the transformation functions Join, Groovy Join, Merge and Groovy
    Merge.
    
    The previous default handlers `IndexJoinHandler` and `IndexMergeHandler` have
    demonstrated negative impact on the performance of some transformations. Until
    this is better understood and fixed, these handlers are now optional and can be
    activated via the following Java properties and environment variables:
    
    - Join/Groovy Join: `hale.functions.use_index_join_handler` / `HALE_FUNCTIONS_USE_INDEX_JOIN_HANDLER`
    - Merge/Groovy Merge: `hale.functions.use_index_merge_handler` / `HALE_FUNCTIONS_USE_INDEX_MERGE_HANDLER`
    
    ING-4464

--- cst/plugins/eu.esdihumboldt.cst.functions.groovy/src/eu/esdihumboldt/cst/functions/groovy/GroovyMerge.java
+++ cst/plugins/eu.esdihumboldt.cst.functions.groovy/src/eu/esdihumboldt/cst/functions/groovy/GroovyMerge.java
@@ -15,8 +15,11 @@ package eu.esdihumboldt.cst.functions.groovy;
 import java.util.Collection;
 import java.util.List;
 
+import de.fhg.igd.slf4jplus.ALogger;
+import de.fhg.igd.slf4jplus.ALoggerFactory;
 import eu.esdihumboldt.cst.functions.core.Merge;
 import eu.esdihumboldt.cst.functions.core.merge.IndexMergeHandler;
+import eu.esdihumboldt.cst.functions.core.merge.PropertiesMergeHandler;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.functions.MergeFunction;
 import eu.esdihumboldt.hale.common.align.model.impl.PropertyEntityDefinition;
@@ -35,6 +38,11 @@ import eu.esdihumboldt.hale.common.instance.index.InstanceIndexContribution;
 public class GroovyMerge extends GroovyRetype implements MergeFunction, InstanceIndexContribution {
 
 	/**
+	 * The log
+	 */
+	private static final ALogger LOG = ALoggerFactory.getLogger(GroovyMerge.class);
+
+	/**
 	 * The function ID. Not named <code>ID</code> to avoid shadowing
 	 * {@link MergeFunction#ID}.
 	 */
@@ -47,7 +55,23 @@ public class GroovyMerge extends GroovyRetype implements MergeFunction, Instance
 
 	@Override
 	public InstanceHandler<? super TransformationEngine> getInstanceHandler() {
-		return new IndexMergeHandler();
+		boolean useIndexMergeHandler = false;
+
+		String setting = System.getProperty("hale.functions.use_index_merge_handler");
+
+		if (setting == null) {
+			setting = System.getenv("HALE_FUNCTIONS_USE_INDEX_MERGE_HANDLER");
+		}
+
+		if (setting != null) {
+			try {
+				useIndexMergeHandler = Boolean.valueOf(setting);
+			} catch (Throwable e) {
+				LOG.error("Error applying index merge handler setting: " + setting, e);
+			}
+		}
+
+		return useIndexMergeHandler ? new IndexMergeHandler() : new PropertiesMergeHandler();
 	}
 
 	/**
