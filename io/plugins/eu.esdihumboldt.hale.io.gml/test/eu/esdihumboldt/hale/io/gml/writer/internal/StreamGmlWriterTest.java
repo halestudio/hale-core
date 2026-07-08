6405efdab66c778adedc34561732699dca9d1d9a
Simon Templer <simon@wetransform.to>
2026-07-08 16:14:21 +0200

    style: apply formatting conventions

--- io/plugins/eu.esdihumboldt.hale.io.gml/test/eu/esdihumboldt/hale/io/gml/writer/internal/StreamGmlWriterTest.java
+++ io/plugins/eu.esdihumboldt.hale.io.gml/test/eu/esdihumboldt/hale/io/gml/writer/internal/StreamGmlWriterTest.java
@@ -123,10 +123,11 @@ public class StreamGmlWriterTest extends AbstractPlatformTest {
 //		Map<List<String>, Object> values = new HashMap<List<String>, Object>();
 //
 //		// create the geometry
-	////		MultiLineString mls = geomFactory.createMultiLineString(
-////				new LineString[]{createLineString(0.0), createLineString(1.0),
-////						createLineString(2.0)});
-//		//XXX for some reason the MultiLineString is converted to a LineString when set a value -> so we are using a LineString instead to allow value comparison
+	//// MultiLineString mls = geomFactory.createMultiLineString( / new
+	/// LineString[]{createLineString(0.0), createLineString(1.0), /
+	/// createLineString(2.0)}); //XXX for some reason the MultiLineString is
+	/// converted to a LineString when set a value -> so we are using a LineString
+	/// instead to allow value comparison
 //		LineString mls = createLineString(0.0);
 //
 //		values.put(Arrays.asList("LENGTH"), Double.valueOf(10.2)); //$NON-NLS-1$
